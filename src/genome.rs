909acc7660ff36d983cd2f75b45de8b737c7c40f
Aaron Sullivan <aasullivan1618@gmail.com>
2023-01-11 22:37:18 -0800

    Get some game plan / data structures made for genome creation
    For the neural net calculations, there's now a thorough game plan and a
    set of data structures that'll make it hopefully easy to create the
    genomes in a way that doesn't involve having to check retroactively to
    ensure the genome is valid. It'll be guaranteed valid.
    
    This will save time in each new random lifeform creation.
    (Assuming copying a ~400 member hashset of (usize, usize) is faster than
     checking and redoing was on average)
    
    But more importantly it'll save from evolving over lifeforms that were
    never going to do anything anyways.

--- src/genome.rs
+++ src/genome.rs
@@ -58,16 +58,74 @@ pub struct GenomeProps<'a> {
 }
 
 pub struct Genome {
-    input_genes: Vec<Gene>,  // All the genes coming only from input neurons
-    inner_genes: Vec<Gene>,  // All the genes that go from inner neurons to inner neurons
-    output_genes: Vec<Gene>, // All the genes that go from inner neurons to output neurons
+    /// All the genes coming only from input neurons
+    /// (this is here to facilitate the neural net calculation at each tic)
+    pub input_genes: Vec<Gene>,
+    /// All the genes that go from inner neurons to inner neurons
+    /// (this is here to facilitate the neural net calculation at each tic)
+    pub inner_genes: Vec<Gene>,
+    /// All the genes that go from inner neurons to output neurons
+    /// (this is here to facilitate the neural net calculation at each tic)
+    pub output_genes: Vec<Gene>,
 }
 
 impl Genome {
     pub fn new(props: GenomeProps) -> Self {
-        let input_neuron_ids: Vec<usize> = props.neural_net.input_neurons.keys().map(|k| *k).collect();
-        let inner_neuron_ids: Vec<usize> = props.neural_net.inner_neurons.keys().map(|k| *k).collect();
-        let output_neuron_ids: Vec<usize> = props.neural_net.output_neurons.keys().map(|k| *k).collect();
+        let input_neuron_ids: Vec<usize> =
+            props.neural_net.input_neurons.keys().map(|k| *k).collect();
+        let inner_neuron_ids: Vec<usize> =
+            props.neural_net.inner_neurons.keys().map(|k| *k).collect();
+        let output_neuron_ids: Vec<usize> =
+            props.neural_net.output_neurons.keys().map(|k| *k).collect();
+
+        // These will allow us to ensure we're picking unique pairs
+        // Can turn each into iter(), then can call nth(0) on the iterator, which is like
+        // shifting a value out of the front. It's in an arbitrary order, so this is akin
+        // to taking out one random value from the hashset.
+        // TODO These have to be made one time and when the neural net is first made and cloned or something
+        // AND The neural net should be made one time when the program starts and THAT should be
+        // cloned as well. It can live on NeuralNet::net and NeuralNet::hash_sets or something.
+
+        // (input_neuron_id, output_neuron_id)
+        let input_output: HashSet<(usize, usize)> = HashSet::new();
+
+        for input_neuron_id in input_neuron_ids {
+            for output_neuron_id in output_neuron_ids {
+                input_output.insert((input_neuron_id, output_neuron_id));
+            }
+        }
+
+        // (inner_neuron_id, output_neuron_id)
+        let inner_output: HashSet<(usize, usize)> = HashSet::new();
+
+        for inner_neuron_id in inner_neuron_ids {
+            for output_neuron_id in output_neuron_ids {
+                inner_output.insert((inner_neuron_id, output_neuron_id));
+            }
+        }
+
+        let inner_output_neuron_ids = inner_neuron_ids.clone();
+        inner_output_neuron_ids.append(&mut output_neuron_ids.clone());
+
+        // (input_neuron_id, inner_neuron_id AND output_neuron_id)
+        let input_inner_output: HashSet<(usize, usize)> = HashSet::new();
+
+        for input_neuron_id in input_neuron_ids {
+            for inner_output_neuron_id in inner_output_neuron_ids {
+                input_inner_output.insert((input_neuron_id, inner_output_neuron_id));
+            }
+        }
+
+        // (inner_neuron_id, inner_neuron_id AND output_neuron_id)
+        let inner_inner_output: HashSet<(usize, usize)> = HashSet::new();
+
+        for inner_neuron_id in inner_neuron_ids {
+            for inner_output_neuron_id in inner_output_neuron_ids {
+                inner_inner_output.insert((inner_neuron_id, inner_output_neuron_id));
+            }
+        }
+
+        // --- End of hashset creation --- //
 
         let input_genes = vec![];
         let inner_genes = vec![];
@@ -76,16 +134,45 @@ impl Genome {
         // from id, to id
         let duplicate_prevention_set: HashSet<(usize, usize)> = HashSet::new();
 
+        // TODO Right now we're validating the genes heavily within the lifeform.
+        // Instead, I'd much rather proactively create a valid structure here.
+        //
+        // To do that:
+        // * If we have a starting id (it's an inner neuron):
+        //  * If there's only one gene left:
+        //   * Pick randomly from output neurons and make that TO.
+        //   * Add that to output_genes;
+        //   * break;
+        //  * If there're more genes left to make:
+        //   * Pick randomly from inner neurons AND output neurons
+        //   * If it's an inner neuron
+        //    * Add gene to inner_genes
+        //    * assign neuron's id to starting id
+        //    * continue;
+        //   * If it's an output neuron
+        //    * Add gene to output_genes
+        //    * continue;
+        // * If we don't have a starting id:
+        //  * If there's only one gene left:
+        //   * Pick random pair from input_output
+        //   * Add that to output_genes;
+        //   * break;
+        //  * If there're more genes to make:
+        //   * Pick random pair from input_inner_output.
+        //   * If TO is an inner neuron:
+        //    * Add it to inner_genes
+        //    * Set starting id to TO (inner neuron)
+        //    * continue;
+        //   * If TO is an output neuron:
+        //    * Add it to output_genes
+        //    * continue;
+
         // To prevent duplicate genes.
         // TODO This isn't the most efficient practice. If the number of
         // genes starts to approach the number of neurons we have, we're going to start having
         // a lot of collisions and this could take who knows long.
         while duplicate_prevention_set.len() < props.size {
-            let gene = Gene::new_random(
-                input_neuron_ids,
-                inner_neuron_ids,
-                output_neuron_ids,
-            );
+            let gene = Gene::new_random(input_neuron_ids, inner_neuron_ids, output_neuron_ids);
 
             let is_unique = duplicate_prevention_set.insert((gene.from, gene.to));
 
--- src/lifeform.rs
+++ src/lifeform.rs
@@ -21,6 +21,17 @@ impl LifeForm {
 
         let mut genome = Genome::new(genome_props);
 
+        // TODO This whole loop is gonna be gone and the bs processed_genome method
+        // is kaputz and Genome::new is going to take care of all of that efficiently.
+        loop {
+            let (is_valid, processed_genome) = LifeForm::process_genome(genome);
+
+            if is_valid {
+                genome = processed_genome;
+                break;
+            }
+        }
+
         Self {
             id,
             genome,
@@ -32,6 +43,35 @@ impl LifeForm {
         }
     }
 
+    // TODO This is also inefficient. If this could be moved to genome and proactively
+    // built up instead of retroactively enforced we'd save statistically some time on lifeform
+    // generation.
+    // Returns a tuple of whether the genome is valid, and the genome itself.
+    // Processing will happen on the genome to remove genes that will have no
+    // effect.
+    fn process_genome(neural_net: &NeuralNet, genome: Genome) -> (bool, Genome) {
+        // * Remove all genes that come from an inner neuron that has no input.
+        // * If there are no genes left, lifeform is invalid.
+
+        // If there are no genes from input neurons OR no genes to
+        // output neurons, the lifeform is invalid.
+        if genome.input_genes.len() == 0 {
+            return (false, genome);
+        }
+
+        if genome.output_genes.len() == 0 {
+            return (false, genome);
+        }
+
+        // Remove all genes that lead to an inner neuron that has no output (no genes have a from
+        // for that inner neuron)
+        // Go through each inner neuron in the neural net:
+        // * If there're no genes with a FROM value of the neuron's id:
+        //  * Remove all genes with a TO value to that neuron's id
+        genome.out
+
+    }
+
     /// returns a list of probablities associated with output neuron types
     pub fn calculate_output_probabilities(&self) -> Vec<(OutputNeuronType, f32)> {
         // * let running_sums = HashMap<neuron_id, sum>
@@ -39,12 +79,6 @@ impl LifeForm {
 
         // TODO in Genome
         // First, we validate:
-        // * If there are no genes from input neurons OR no genes to
-        // output neurons, the lifeform is invalid.
-        // * Remove all genes that lead to an inner neuron that has no output (no genes have a from
-        // from that inner neuron)
-        // * Remove all genes that come from an inner neuron that has no input.
-        // * If there are no genes left, lifeform is invalid.
         //
         // Game plan:
         // * For each gene from an input neuron (no matter its destination is inner or output):
