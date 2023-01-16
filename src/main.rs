10655765a95a90783e7e0aad4af5e059ccbb5845
Aaron Sullivan <aasullivan1618@gmail.com>
2023-01-15 18:23:08 -0800

    Got the stats panel back up and running

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use tui::{
     layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Style},
     text::Span,
-    widgets::{canvas::Canvas, Block, Borders, Paragraph, Widget},
+    widgets::{canvas::Canvas, Block, Borders, Paragraph, Widget, ListItem, List},
     Frame, Terminal,
 };
 
@@ -66,7 +66,14 @@ fn main() {
                 match key.code {
                     KeyCode::Char('q') => break,
                     KeyCode::Char('p') => paused = !paused,
-                    _ => ()
+
+                    // TODO here could pause this loop, call a fn that has another
+                    // loop that just steps. In that fn though need to figure out
+                    // how to capture key events.
+                    // Alternatively, could have e mean do like 10,000 frames or something
+                    // without UI. So like a quick jump into the future.
+                    KeyCode::Char('e') => todo!(),
+                    _ => (),
                 };
             }
         }
@@ -119,7 +126,7 @@ where
         .split(area);
 
     draw_world(f, size, world, chunks[0]);
-    draw_right(f, chunks[1]);
+    draw_right(f, world, chunks[1]);
 }
 
 fn draw_world<B>(f: &mut Frame<B>, size: usize, world: &World, area: Rect)
@@ -165,6 +172,7 @@ where
                     _ => Color::White,
                 };
 
+                // TODO add_modifier Modifier::BOLD
                 ctx.print(
                     lf.location.0 as f64,
                     lf.location.1 as f64,
@@ -205,15 +213,13 @@ where
     B: Backend,
 {
     let block = Block::default().title("Controls").borders(Borders::ALL);
-    let paragraph = Paragraph::new(format!(
-        "controls: q = quit | p = pause | f = change frame rate | e = evolve without UI | iteration {}",
-    iteration))
-    .block(block);
+    let text = format!( "controls: q = quit | p = pause | f = change frame rate | e = evolve without UI | iteration {}", iteration);
+    let paragraph = Paragraph::new(text).block(block);
 
     f.render_widget(paragraph, area);
 }
 
-fn draw_right<B>(f: &mut Frame<B>, area: Rect)
+fn draw_right<B>(f: &mut Frame<B>, world: &World, area: Rect)
 where
     B: Backend,
 {
@@ -223,16 +229,46 @@ where
         .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
         .split(area);
 
-    draw_stats(f, chunks[0]);
+    draw_stats(f, world, chunks[0]);
     draw_events(f, chunks[1]);
 }
 
-fn draw_stats<B>(f: &mut Frame<B>, area: Rect)
+fn draw_stats<B>(f: &mut Frame<B>, world: &World, area: Rect)
 where
     B: Backend,
 {
-    let stats_block = Block::default().title("Stats").borders(Borders::ALL);
-    f.render_widget(stats_block, area);
+    let stats: Vec<(usize, usize, f32, f32, f32, (usize, usize))> = world
+        .lifeforms
+        .values()
+        .map(|lf| {
+            (
+                lf.id,
+                lf.lifespan,
+                lf.health,
+                lf.hunger,
+                lf.thirst,
+                lf.location,
+            )
+        })
+        .collect();
+
+    let mut lis: Vec<ListItem> = vec![ListItem::new(Span::from("Stats: id, lifespan, health, hunger, thirst, location"))];
+
+    for lf in world.lifeforms.values() {
+        let stat = ( lf.id, lf.lifespan, lf.health, lf.hunger, lf.thirst, lf.location);
+        lis.push(ListItem::new(Span::from(format!("{:.10?}", stat))));
+    }
+
+    let list = List::new(lis)
+        .block(Block::default().title("Stats").borders(Borders::ALL));
+
+    // // Stats
+    // screen.print(1, 0,);
+    // for (idx, stat) in stats.iter().enumerate() {
+    //     screen.print(1, idx as i32, &format!("{:.10?}", stat));
+    // }
+
+    f.render_widget(list, area);
 }
 
 fn draw_events<B>(f: &mut Frame<B>, area: Rect)
@@ -242,62 +278,6 @@ where
     let block = Block::default().title("Events").borders(Borders::ALL);
     f.render_widget(block, area);
 }
-
-// // TODO ATTOW there's an error in console_engine that disallows any value over 1000 for
-// // target_fps. If this becomes a real issue we can switch back to the normal way instead
-// // of engine.poll() way. However it'd also be really nice to add an escape hatch to run
-// // the evolution and not show anything on the screen.
-// // This is documented here: https://github.com/VincentFoulon80/console_engine/issues/17
-// loop {
-//     // Poll next event
-//     match engine.poll() {
-//         // A frame has passed
-//         Event::Frame => {
-//             if !paused {
-//                 engine.clear_screen();
-//                 for screen in [
-//                     &mut main_screen,
-//                     &mut controls_screen,
-//                     &mut stats_screen,
-//                     &mut info_screen,
-//                 ] {
-//                     screen.clear();
-//                 }
-
-//                 world.step();
-
-//                 // update_main_screen(&mut main_screen, &world);
-//                 // update_stats_screen(&mut stats_screen, &world);
-
-//                 // engine.print_screen(0, 0, &main_screen);
-//                 // engine.print_screen(0, (engine.get_height() - 1) as i32, &controls_screen);
-//                 // engine.print_screen((size + 2) as i32, 0, &stats_screen);
-//             }
-//         }
-
-//         // A Key has been pressed
-//         Event::Key(keyevent) => {
-//             if keyevent.code == KeyCode::Char('q') {
-//                 break;
-//             }
-
-//             if keyevent.code == KeyCode::Char('p') {
-//                 paused = !paused;
-//             }
-
-//             if keyevent.code == KeyCode::Char('f') {
-//                 todo!();
-//             }
-
-//             if keyevent.code == KeyCode::Char('e') {
-//                 todo!();
-//                 // TODO here could pause this loop, call a fn that has another
-//                 // loop that just steps. In that fn though need to figure out
-//                 // how to capture key events.
-//                 // Alternatively, could have e mean do like 10,000 frames or something
-//                 // without UI. So like a quick jump into the future.
-//             }
-//         }
 
 //         // Mouse has been moved or clicked
 //         Event::Mouse(mouseevent) => {
@@ -311,11 +291,6 @@ where
 //                 }
 //             }
 //         }
-
-//         // Window has been resized
-//         Event::Resize(_w, _h) => { /* ... */ }
-//     }
-// }
 
 // fn update_info_screen(lf: &LifeForm, nnh: &NeuralNetHelper, screen: &mut Screen) {
 //     // Clear the screen part that we're using
@@ -346,73 +321,7 @@ where
 //     screen.draw();
 // }
 
-// fn update_main_screen(screen: &mut Screen, world: &World) {
-//     let mut num_at_location: HashMap<(usize, usize), usize> = HashMap::new();
-
-//     for lf in world.lifeforms.values() {
-//         *num_at_location.entry(lf.location).or_insert(0) += 1;
-//         let num = num_at_location[&lf.location];
-
-//         let char = match num {
-//             1 if lf.health >= 0.5 => '☺',
-//             1 if lf.health < 0.5 => '☹',
-//             2 => '2',
-//             3 => '3',
-//             4 => '4',
-//             5 => '5',
-//             6 => '6',
-//             7 => '7',
-//             8 => '8',
-//             9 => '9',
-//             _ => '!',
-//         };
-
-//         let color = match lf.health {
-//             _ if lf.health >= 0.9 => Color::Green,
-//             _ if lf.health >= 0.8 => Color::DarkGreen,
-//             _ if lf.health >= 0.7 => Color::Blue,
-//             _ if lf.health >= 0.6 => Color::DarkBlue,
-//             _ if lf.health >= 0.5 => Color::DarkMagenta,
-//             _ if lf.health >= 0.4 => Color::Magenta,
-//             _ if lf.health >= 0.3 => Color::DarkYellow,
-//             _ if lf.health >= 0.2 => Color::Yellow,
-//             _ if lf.health >= 0.1 => Color::DarkRed,
-//             _ if lf.health < 0.1 => Color::Red,
-//             _ => Color::White,
-//         };
-
-//         screen.set_pxl(
-//             lf.location.0 as i32,
-//             lf.location.1 as i32,
-//             pixel::pxl_fg(char, color),
-//         );
-//     }
-
-//     screen.draw();
-// }
-
 // fn update_stats_screen(screen: &mut Screen, world: &World) {
-//     let stats: Vec<(usize, usize, f32, f32, f32, (usize, usize))> = world
-//         .lifeforms
-//         .values()
-//         .map(|lf| {
-//             (
-//                 lf.id,
-//                 lf.lifespan,
-//                 lf.health,
-//                 lf.hunger,
-//                 lf.thirst,
-//                 lf.location,
-//             )
-//         })
-//         .collect();
-
-//     // Stats
-//     screen.line(0, 0, 0, (screen.get_height() - 2) as i32, pixel::pxl('|'));
-//     screen.print(1, 0, "Stats: id, lifespan, health, hunger, thirst");
-//     for (idx, stat) in stats.iter().enumerate() {
-//         screen.print(1, idx as i32, &format!("{:.10?}", stat));
-//     }
 
 //     screen.draw();
 // }
