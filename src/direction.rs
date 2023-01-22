Aaronik/evolution

commit 9d168aa81b240cda7ca280ff37357253fe890981

Now the danger hunts down the little guys

src/util.rs
@@ -92,51 +92,46 @@ pub fn dist_rel(world_size: usize, from: &(usize, usize), to: &(usize, usize)) -
     dist_abs(from, to) / farthest_possible
 }
 
-/// Returns
-/// 0.25 for north
-/// 0.50 for east
-/// 0.75 for south
-/// 1.00 for west
-/// 0.00 for same point
+/// Returns one of the four cardinal directions, and returns north if it's the same point.
 /// And assumes that the start point is in the bottom left corner.
-pub fn direc(from: &(usize, usize), to: &(usize, usize)) -> f32 {
+pub fn direc(from: &(usize, usize), to: &(usize, usize)) -> DirectionName {
     let x1 = from.0 as f32;
     let y1 = from.1 as f32;
     let x2 = to.0 as f32;
     let y2 = to.1 as f32;
 
-    if y2 < y1 && y2 + y1 > x2 + x1 {
-        return 0.25;
+    if y2 > y1 && y2 + y1 > x2 + x1 {
+        return DirectionName::North;
     } else if x2 > x1 && x2 + x1 > y2 + y1 {
-        return 0.5;
-    } else if y2 > y1 && y2 + y1 > x2 + x1 {
-        return 0.75;
+        return DirectionName::East;
+    } else if y2 < y1 && y2 + y1 > x2 + x1 {
+        return DirectionName::South;
     } else if x2 < x1 && x2 + x1 > y2 + y1 {
-        return 1.0;
+        return DirectionName::West;
     }
 
     if x2 == x1 {
-        if y2 > y1 {
+        if y2 < y1 {
             // straight down
-            return 0.75;
-        } else if y2 < y1 {
+            return DirectionName::South;
+        } else if y2 > y1 {
             // straight up
-            return 0.25;
+            return DirectionName::North;
         }
     }
 
     if y2 == y1 {
         if x2 > x1 {
             // straight right
-            return 0.5;
+            return DirectionName::East;
         } else if x2 < x1 {
             // straight left
-            return 1.0;
+            return DirectionName::West;
         }
     }
 
     // Otherwise it's the same point
-    0.0
+    DirectionName::North
 }
 
 /// Relocate one step randomly
@@ -253,6 +248,18 @@ pub fn rel_dir(
 
     // Now convert to a unit digit
     radians / std::f32::consts::PI
+}
+
+/// Takes a mutable subject and moves it one step towards a given object, each being a location
+/// TODO Currently only does cardinal directions but could also be updated to get all 8.
+pub fn move_towards(size: usize, subject: &mut (usize, usize), object: &(usize, usize)) {
+    match direc(subject, object) {
+        DirectionName::North => update_location(size, subject, &(0, 1)),
+        DirectionName::East => update_location(size, subject, &(1, 0)),
+        DirectionName::South => update_location(size, subject, &(0, -1)),
+        DirectionName::West => update_location(size, subject, &(-1, 0)),
+        _ => ()
+    }
 }
 
 #[cfg(test)]

src/main.rs
@@ -33,7 +33,7 @@ fn main() {
         neural_net_helper: &nnh,
         num_initial_lifeforms: 20,
         genome_size: 25,
-        mutation_rate: 0.001,
+        mutation_rate: 0.1,
         food_density: 30,
         num_inner_neurons,
         minimum_number_lifeforms: 15,
@@ -53,7 +53,7 @@ fn main() {
     // When we pause we greatly increase the tick rate to keep the loop from
     // cooking the CPUs. This is where we store the value to go back to.
     // Note we mutate this to adjust tick rate.
-    let mut saved_tick_rate = 250;
+    let mut saved_tick_rate = 0;
 
     // Will be adjusted within the loop as well
     let mut paused = false;

src/world.rs
@@ -116,9 +116,13 @@ impl<'a> World<'a> {
             }
         }
 
-        // Let the danger move around slowly
+        // Let the danger hunt
         if self.tics % self.props.danger_delay == 0 {
-            randomize(self.props.size, &mut self.danger);
+            let closest_lf_loc = closest_to(
+                &self.danger,
+                &self.lifeforms.iter().map(|(_, lf)| lf.location).collect(),
+            );
+            move_towards(self.props.size, &mut self.danger, &closest_lf_loc);
         }
 
         for lf_id in has_died {

commit 1434029b8e3a2b691277c33dbb0570b3077c6fa7

Woops, broke the tests with the danger hunt commit
9d168aa81b240cda7ca280ff37357253fe890981 broke the tests, this fixes
them

src/direction.rs
@@ -57,7 +57,7 @@ impl Direction {
 // TODO Still I'm feeling like with the numbers at least we should be able to switch.
 // So basically I want to say, from turn left, "return a Me minus one". Then get_forward_modifier
 // could be basically the same.
-#[derive(Debug, EnumIter, Clone)]
+#[derive(Debug, EnumIter, Clone, PartialEq)]
 pub enum DirectionName {
     North = 0,
     NorthEast = 1,

src/util.rs
@@ -452,47 +452,47 @@ mod tests {
         // Same point
         let l1 = (0, 0);
         let l2 = (0, 0);
-        assert_eq!(direc(&l1, &l2), 0.0);
+        assert_eq!(direc(&l1, &l2), DirectionName::North);
 
         let l1 = (5, 5);
         let l2 = (5, 5);
-        assert_eq!(direc(&l1, &l2), 0.0);
+        assert_eq!(direc(&l1, &l2), DirectionName::North);
 
         // Generally north
-        let l1 = (0, 1);
-        let l2 = (0, 0);
-        assert_eq!(direc(&l1, &l2), 0.25);
+        let l1 = (0, 0);
+        let l2 = (0, 1);
+        assert_eq!(direc(&l1, &l2), DirectionName::North);
 
-        let l1 = (1, 3);
-        let l2 = (0, 0);
-        assert_eq!(direc(&l1, &l2), 0.25);
+        let l1 = (0, 0);
+        let l2 = (1, 3);
+        assert_eq!(direc(&l1, &l2), DirectionName::North);
 
         // Generally east
         let l1 = (0, 0);
         let l2 = (1, 0);
-        assert_eq!(direc(&l1, &l2), 0.50);
+        assert_eq!(direc(&l1, &l2), DirectionName::East);
 
         let l1 = (0, 1);
         let l2 = (5, 2);
-        assert_eq!(direc(&l1, &l2), 0.50);
+        assert_eq!(direc(&l1, &l2), DirectionName::East);
 
         // Generally south
-        let l1 = (0, 0);
-        let l2 = (0, 1);
-        assert_eq!(direc(&l1, &l2), 0.75);
+        let l1 = (0, 1);
+        let l2 = (0, 0);
+        assert_eq!(direc(&l1, &l2), DirectionName::South);
 
-        let l1 = (0, 0);
-        let l2 = (1, 2);
-        assert_eq!(direc(&l1, &l2), 0.75);
+        let l1 = (1, 2);
+        let l2 = (0, 0);
+        assert_eq!(direc(&l1, &l2), DirectionName::South);
 
         // Generally west
         let l1 = (1, 0);
         let l2 = (0, 0);
-        assert_eq!(direc(&l1, &l2), 1.00);
+        assert_eq!(direc(&l1, &l2), DirectionName::West);
 
         let l1 = (3, 2);
         let l2 = (0, 0);
-        assert_eq!(direc(&l1, &l2), 1.00);
+        assert_eq!(direc(&l1, &l2), DirectionName::West);
     }
 
     #[test]